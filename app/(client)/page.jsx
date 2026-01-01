1a4cfda29b037954d7b049df153818b6d700c205
dhuka muhammad <dhukamohammad5@gamil.com>
2026-01-01 09:57:31 +0530

    donation app

---
+++ app/(client)/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <section className="container mx-auto px-4 py-10">
+            <h1>Welcome to the Donation App</h1>
+        </section>
+    );
+}
