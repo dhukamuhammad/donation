2677c98d1051ec3914c3b5473944d0691949f556
Dhukkahafiz <hafizdhukka431@gmail.com>
2026-01-10 11:06:52 +0530

    change details page

--- app/api/donationFund/by-fund/[id]/route.js
+++ app/api/donationFund/by-fund/[id]/route.js
@@ -2,25 +2,20 @@ import db from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET(req, { params }) {
-    try {
-        const { id } = await params;
+  try {
+    const { id } = await params;
 
-        const [rows] = await db.query(
-            `
-      SELECT 
-        IFNULL(SUM(amount), 0) AS raised_amount,
-        COUNT(id) AS supporters
+    const [rows] = await db.query(
+      `
+      SELECT *
       FROM donation_info
       WHERE fund_id = ?
       `,
-            [id]
-        );
+      [id]
+    );
 
-        return NextResponse.json(rows[0]);
-    } catch (error) {
-        return NextResponse.json(
-            { error: error.message },
-            { status: 500 }
-        );
-    }
+    return NextResponse.json(rows);
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
